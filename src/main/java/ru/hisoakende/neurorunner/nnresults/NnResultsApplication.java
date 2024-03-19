024892182f67a6f3b89400335db5459369a99917
hisoakende <hisoakende@gmail.com>
2024-03-19 19:29:09 +0300

    Init

---
+++ src/main/java/ru/hisoakende/neurorunner/nnresults/NnResultsApplication.java
@@ -0,0 +1,13 @@
+package ru.hisoakende.neurorunner.nnresults;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NnResultsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NnResultsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/hisoakende/neurorunner/nnresults/NnResultsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hisoakende.neurorunner.nnresults;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NnResultsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
