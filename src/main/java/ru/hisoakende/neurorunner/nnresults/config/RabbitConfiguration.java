e89f9c5c1bdd778f0d931acd9271b707816f1b94
Zorov Maxim <zorov.ma@mail.ru>
2024-04-04 16:28:53 +0300

    Add configuration files

---
+++ src/main/java/ru/hisoakende/neurorunner/nnresults/config/RabbitConfiguration.java
@@ -0,0 +1,36 @@
+package ru.hisoakende.neurorunner.nnresults.config;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitConfiguration {
+
+    @Value("${spring.rabbitmq.host}")
+    private String host;
+    @Value("${spring.rabbitmq.port}")
+    private int port;
+    @Value("${spring.rabbitmq.username}")
+    private String userName;
+    @Value("${spring.rabbitmq.password}")
+    private String password;
+
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
+        connectionFactory.setHost(host);
+        connectionFactory.setPort(port);
+        connectionFactory.setUsername(userName);
+        connectionFactory.setPassword(password);
+        return connectionFactory;
+    }
+
+    @Bean
+    public Queue myQueue() {
+        return new Queue("rabbitQueue");
+    }
+}
